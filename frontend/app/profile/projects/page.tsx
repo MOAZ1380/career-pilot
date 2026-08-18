3c09bc70f5bf9d88f2e3c963f26f5e3f19ac5400
Moaz Sayed <moaz.sayed.said@gmail.com>
2026-08-18 20:50:13 +0300

    feat(projects): implement project management API, types, schema and UI

---
+++ frontend/app/profile/projects/page.tsx
@@ -0,0 +1,9 @@
+import ProjectForm from "@/services/project/components/ProjectForm";
+
+export default function ProjectsPage() {
+  return (
+    <main className="min-h-screen px-4 py-10 flex justify-center items-center">
+      <ProjectForm />
+    </main>
+  );
+}
