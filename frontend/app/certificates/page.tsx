150b4eebb899a6b9a9332c84d156f9b22e12c2e3
unknown <abdallaheassa1234@gmail.com>
2026-08-22 12:53:45 +0300

    add certificate

---
+++ frontend/app/certificates/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import CertificateForm from "../../services/certificate/components/CertificateForm"
+import CertificatesForm from "../../services/certificate/components/CertificatesForm"
+
+export default function CertificatesPage() {
+  return (
+    <>
+    
+    <CertificatesForm/></>
+  )
+}
